e7880925c8fe4fd8239f98808a51d38c443be563
Eduardo Gurgel <eduardo-gurgel@hotmail.com>
2024-08-03 10:26:29 -0300

    Plotando posições

--- run_simulator.py
+++ run_simulator.py
@@ -1,12 +1,53 @@
+import matplotlib.pyplot as plt
 import time
 from futebol_model import FutebolModel
 
-duracao_jogo = 90
+def plot_positions(ax, model):
+    ax.clear()
+    
+    agent_positions = model.get_agent_positions()
+    
+    ax.set_xlim(0, model.grid.width)
+    ax.set_ylim(0, model.grid.height)
+    
+    for pos in agent_positions["Jogador"]:
+        x, y = pos
+        ax.scatter(x, y, c='blue', marker='o', label='Jogador')
 
-model = FutebolModel(duracao_jogo)
+    for pos in agent_positions["Bola"]:
+        x, y = pos
+        ax.scatter(x, y, c='black', marker='x', label='Bola')
 
-while model.running:
-    model.step()
-    time.sleep(0.5)
+    for pos in agent_positions["Arbitro"]:
+        x, y = pos
+        ax.scatter(x, y, c='red', marker='s', label='Arbitro')
 
-print("O jogo acabou!")
+    ax.set_title(f'Tempo decorrido: {model.tempo_decorrido} minuto(s)')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.grid(True)
+
+def main():
+    duracao_jogo = 90
+    model = FutebolModel(duracao_jogo)
+    
+    plt.ion()  
+    fig, ax = plt.subplots(figsize=(10, 10))
+    
+    def on_close(event):
+        model.running = False
+    
+    fig.canvas.mpl_connect('close_event', on_close)
+    
+    while model.running:
+        model.step()
+        plot_positions(ax, model)
+        plt.draw()
+        plt.pause(0.5)  
+
+    plt.ioff()  
+    plt.show()
+    print("O jogo acabou!")
+
+if __name__ == "__main__":
+    main()
